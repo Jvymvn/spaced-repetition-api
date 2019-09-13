7f5a1f30e1320189bd0063c5d40bf0ddb3d4b1e3
Jvymvn <Jordon.carter@yahoo.com>
2019-09-12 22:57:11 -0400

    Tests

--- src/language/language-router.js
+++ src/language/language-router.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const LanguageService = require('./language-service')
 const { requireAuth } = require('../middleware/jwt-auth')
-
+const bodyParser = express.json()
 const languageRouter = express.Router()
 
 languageRouter
@@ -62,9 +62,68 @@ languageRouter
   });
 
 languageRouter
-  .post('/guess', async (req, res, next) => {
-    // implement me
-    res.send('implement me!')
+  .post('/guess', bodyParser, async (req, res, next) => {
+    if (!Object.keys(req.body).includes('guess')) {
+      return res.status(400).json({
+        error: `Missing 'guess' in request body`
+      });
+    }
+
+    const words = await LanguageService.getLanguageWords(
+      req.app.get('db'),
+      req.language.id
+    );
+
+    const wordList = LanguageService.addListWords(
+      req.language,
+      words
+    );
+
+    // console.log(req.body.guess)
+    // console.log(wordList.head)
+
+    console.log(req.body.guess)
+
+
+    if (req.body.guess === wordList.head.value.translation) {
+      wordList.head.value.correct_count++;
+
+      wordList.head.value.memory_value =
+        (wordList.head.value.memory_value * 2 >= wordList.listNodes().length
+          ? wordList.listNodes().length - 1
+          : wordList.head.value.memory_value * 2);
+
+      wordList.total_score++;
+      wordList.moveHeadBy(wordList.head.value.memory_value);
+
+      LanguageService.afterLinkList(req.app.get('db'), wordList).then(() => {
+        res.json({
+          nextWord: wordList.head.value.original,
+          wordCorrectCount: wordList.head.value.correct_count,
+          wordIncorrectCount: wordList.head.value.incorrect_count,
+          totalScore: wordList.total_score,
+          answer: req.body.guess,
+          isCorrect: true,
+        });
+        next();
+      });
+    } else {
+      wordList.head.value.incorrect_count++; // increase incorrect count for curr word
+      wordList.head.value.memory_value = 1; // reset memory value to 1
+      let rightAnswer = wordList.head.value.translation; // store right answer before moving head
+      wordList.moveHeadBy(wordList.head.value.memory_value);
+      LanguageService.afterLinkList(req.app.get('db'), wordList).then(() => {
+        res.json({
+          nextWord: wordList.head.value.original,
+          wordCorrectCount: wordList.head.value.correct_count,
+          wordIncorrectCount: wordList.head.value.incorrect_count,
+          totalScore: wordList.total_score,
+          answer: rightAnswer, // translation is right answer, guess wrong
+          isCorrect: false,
+        });
+        next();
+      });
+    }
   })
 
 module.exports = languageRouter
--- src/language/language-service.js
+++ src/language/language-service.js
@@ -1,3 +1,5 @@
+const LinkedList = require('../LinkedList/LinkedList');
+
 const LanguageService = {
   getUsersLanguage(db, user_id) {
     return db
@@ -28,12 +30,68 @@ const LanguageService = {
       )
       .where({ language_id })
   },
+
   getNextWord(db, id) {
     return db('word')
       .select('id', 'next', 'original', 'correct_count', 'incorrect_count')
       .where({ id })
       .first();
   },
-}
+
+  addListWords(language, words) {
+    let wordList = new LinkedList(); // import linked list
+    wordList.id = language.id;
+    wordList.name = language.name; // french
+    wordList.total_score = language.total_score;
+    let word = words.find(w => w.id === language.head);
+
+    wordList.insertFirst({
+      id: word.id,
+      original: word.original,
+      translation: word.translation,
+      memory_value: word.memory_value,
+      correct_count: word.correct_count,
+      incorrect_count: word.incorrect_count,
+    });
+    while (word.next) {
+      word = words.find(w => w.id === word.next);
+      wordList.insertLast({
+        id: word.id,
+        original: word.original,
+        translation: word.translation,
+        memory_value: word.memory_value,
+        correct_count: word.correct_count,
+        incorrect_count: word.incorrect_count,
+      });
+    }
+    return wordList;
+  },
+
+  afterLinkList(db, linkLanguage) {
+    return db.transaction(trx =>
+      Promise.all([
+        db('language')
+          .transacting(trx)
+          .where('id', linkLanguage.id)
+          .update({
+            total_score: linkLanguage.total_score,
+            head: linkLanguage.head.value.id
+          }),
+
+        ...linkLanguage.forEach(node =>
+          db('word')
+            .transacting(trx)
+            .where('id', node.value.id)
+            .update({
+              memory_value: node.value.memory_value,
+              correct_count: node.value.correct_count,
+              incorrect_count: node.value.incorrect_count,
+              next: node.next ? node.next.value.id : null,
+            })
+        ),
+      ])
+    );
+  },
+};
 
 module.exports = LanguageService
